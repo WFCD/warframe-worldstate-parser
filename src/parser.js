b6f33b92ba0ff8fb6065be3e3b4b36caaef009a9
nspacestd <nspacestd@users.noreply.github.com>
2016-06-07 19:29:25 +0200

    Change everything to async

---
+++ src/parser.js
@@ -0,0 +1,402 @@
+var request = require('request');
+var WorldState = require('./worldstate.js');
+
+var MAX_CACHED_TIME = process.env.WORLDSTATE_CACHE_LENGTH || 300000;
+
+const platformURL = {
+  PC: 'http://content.warframe.com/dynamic/worldState.php',
+  PS4: 'http://content.ps4.warframe.com/dynamic/worldState.php',
+  X1: 'http://content.xb1.warframe.com/dynamic/worldState.php'
+}
+
+var Parser = function (platform) {
+  this.platform = platform;
+  this.cache = null;
+  this.events = null;
+  this.news = null;
+  this.sorties = null;
+  this.globalModifiers = null;
+  this.lastRefresh = null;
+  this.refreshing = false;
+  this.refreshQueue = [];
+}
+
+Parser.prototype.dataIsCurrent = function() {
+  return this.cache &&
+    this.data.creation.getMilliseconds() - Date.now() > MAX_CACHED_TIME
+}
+
+Parser.prototype.getData = function(callback) {
+  if(this.dataIsCurrent()) {
+    callback(null, this.cache);
+  } else {
+    this.refresh(callback);
+  }
+}
+
+Parser.prototype.refresh = function(callback) {
+  var self = this;
+
+  this.refreshQueue.push(callback);
+  if(!this.refreshing) {
+    this.refreshing = true;
+
+    this.retrieve(function(err, data) {
+      if(!err) {
+        self.cache = data;
+        self.lastRefresh = Date.now();
+      }
+      self.refreshing = false;
+      self.processRefreshQueue(err, data);
+    });
+  }
+}
+
+Parser.prototype.retrieve = function(callback) {
+  var url = platformURL[this.platform];
+
+  request.get(url, function(err, response, body) {
+    if(err) {
+      return callback(err);
+    }
+    if(response.statusCode !== 200) {
+      var error
+      error = new Error(url + ' returned HTTP status ' + response.statusCode)
+      return callback(error);
+    }
+    var data
+
+    try {
+      data = JSON.parse(body);
+    } catch(e) {
+      data = null;
+    }
+
+    if(!data) {
+      var error
+      error = new Error('Invalid JSON from ' + url);
+      return callback(error);
+    }
+    callback(null, new WorldState(data));
+  });
+}
+
+Parser.prototype.processRefreshQueue = function(err, data) {
+  while(this.refreshQueue.length) {
+    this.refreshQueue.pop()(err, data);
+  }
+}
+
+//Sortie
+Parser.prototype.getSortie = function(callback){
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.sorties);
+  }); 
+}
+Parser.prototype.getSortieString = function(callback){
+  this.getSortie(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.toString());
+  });
+}
+
+//Conclave
+Parser.prototype.getConclaveDailies = function(callback){
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.conclaveChallenge.getDailies());
+  });
+}
+Parser.prototype.getConclaveDailiesString = function(callback){
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.conclaveChallenge.getDailiesAsString());
+  });
+}
+Parser.prototype.getConclaveWeeklies = function(callback){
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.conclaveChallenge.getConclaveWeeklies());
+  });
+}
+Parser.prototype.getConclaveWeekliesString = function(callback){
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.conclaveChallenge.getWeekliesAsString());
+  });
+}
+Parser.prototype.getConclaveAll = function(callback){
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.conclaveChallenge.getConclaveAll());
+  });
+}
+Parser.prototype.getConclaveAllString = function(callback){
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.conclaveChallenge.getAllAsString());
+  });
+}
+
+//Persistent enemies
+Parser.prototype.getAllPersistentEnemies = function(callback) {
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.enemies.getAll());
+  });
+}
+Parser.prototype.getAllPersistentEnemiesString = function(callback) {
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.enemies.getAllAsString());
+  });
+}
+Parser.prototype.getDisoveredPersistentEnemies = function(callback) {
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.enemies.getDiscovered());
+  });
+}
+Parser.prototype.getDisoveredPersistentEnemiesString = function(callback) {
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.enemies.getDiscoveredAsString());
+  });
+}
+Parser.prototype.getHiddenPersistentEnemies = function(callback) {
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.enemies.getHidden());
+  });
+}
+Parser.prototype.getHiddenPersistentEnemies = function(callback) {
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.enemies.getHiddenAsString());
+  });
+}
+
+//events
+Parser.prototype.getEvents = function(callback) {
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.events.());
+  });
+}
+Parser.prototype.getEventsString = function(callback) {
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.events.toString());
+  });
+}
+
+//news
+Parser.prototype.getNews = function(callback) {
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.news.getAll());
+  });
+}
+Parser.prototype.getNewsString = function(callback) {
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.news.toString());
+  });
+}
+Parser.prototype.getUpdates = function(callback) {
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.news.getUpdates());
+  });
+}
+Parser.prototype.getUpdatesString = function(callback) {
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.news.getUpdatesString());
+  });
+}
+Parser.prototype.getPrimeAccess = function(callback) {
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.news.getPrimeAccess());
+  });
+}
+Parser.prototype.getPrimeAccessString = function(callback) {
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.news.getPrimeAccessString());
+  });
+}
+
+//invasions
+Parser.prototype.getInvasions = function(callback){
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.invasion);
+  });
+}
+Parser.prototype.getInvasionsString = function(callback){
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.invasion.toString());
+  });
+}
+
+//alerts
+Parser.prototype.getAlerts = function(callback) {
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.alerts.getAll());
+  });
+}
+Parser.prototype.getAlertsString = function(callback) {
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.alerts.getAllString());
+  });
+}
+
+//baro
+Parser.prototype.getVoidTrader = function(callback) {
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.voidTrader);
+  });
+}
+Parser.prototype.getVoidTraderString = function(callback) {
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.voidTrader.toString());
+  });
+}
+
+//darvo
+Parser.prototype.getDeals = function(callback){
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.deals);
+  });
+}
+Parser.prototype.getDealsString = function(callback) {
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.deals.toString());
+  });
+}
+
+//flash deals
+Parser.prototype.getFlashDeals = function(callback) {
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.flashDeals);
+  });
+}
+Parser.prototype.getFlashDealsString = function(callback) {
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.flashDeals.toString());
+  });
+}
+
+//Dark sectors
+Parser.prototype.getDarkSectors = function(callback) {
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.darkSectors.getAll());
+  });
+}
+Parser.prototype.getDarkSectorsString = function(callback) {
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.darkSectors.toString());
+  });
+}
+
+//Global Modifiers
+Parser.prototype.getGlobalModifers = function(callback) {
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.globalModifiers.getAll());
+  });
+}
+Parser.prototype.getGlobalModifersString = function(callback) {
+  this.getData(function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+    callback(null, data.globalModifiers.toString());
+  });
+}
+
+module.exports = Parser;
--- src/worldstate.js
+++ src/worldstate.js
@@ -1,5 +1,3 @@
-var request = require('request');
-
 var Events = require('./modules/events.js');
 var News = require('./modules/news.js');
 var Sorties = require('./modules/sortie.js');
@@ -14,31 +12,8 @@ var VoidTrader = require('./modules/baro.js');
 var Deals = require('./modules/deal.js');
 var FlashDeals = require('./modules/flashDeals.js');
 
-var maxCachedTime = process.env.WORLDSTATE_CACHE_LENGTH || 300000;
 
-const platformURL = {
-  PC: 'http://content.warframe.com/dynamic/worldState.php',
-  PS4: 'http://content.ps4.warframe.com/dynamic/worldState.php',
-  X1: 'http://content.xb1.warframe.com/dynamic/worldState.php'
-}
-
-var Worldstate = function (wsplatform) {
-  var platform = wsplatform;
-  var data = undefined;
-  var events = undefined;
-  var news = undefined;
-  var sorties = undefined;
-  var globalModifiers = undefined;
-  var creation = undefined;
-  this.getWorldState(platform, this.setup);
-}
-
-Worldstate.prototype.setup = function(err, data, wsplatform) {
-  if(err) {
-    console.error(err);
-    return;
-  }
-  this.platform = wsplatform;
+WorldState = function(data) {
   this.data = data;
   if(this.data.Goals)
     this.events = new Events(data.Goals);
@@ -48,222 +23,16 @@ Worldstate.prototype.setup = function(err, data, wsplatform) {
   this.conclaveChallenge = new Challenges(data.PVPChallengeInstances);
   this.enemies = new Enemies(data.PersistentEnemies);
 
+  this.creation = Date.now();
+
   /*
   this.darkSectors = new DarkSectors(data.BadlandNodes);
   this.invasion = new Invasion(data.Invasions);
   this.alerts =  new Alerts(data.Alerts);
   this.voidTrader = new VoidTrader(data.VoidTraders[0]);
   this.deals = new Deals(data.DailyDeals);
-  this.flashDeals = new FlashDeals(data.FlashSales);*/
-  this.creation = Date.now();
+  this.flashDeals = new FlashDeals(data.FlashSales);
+  */
 }
 
-Worldstate.prototype.getWorldState = function(platform, callback) {
-  request.get(platformURL[platform], function(err, response, body) {
-    if(err) {
-      console.error(err);
-    }
-    if(response.statusCode !== 200) {
-      var error
-      error = new Error(url + ' returned HTTP status ' + response.statusCode)
-      console.error(error);
-    }
-    var data
-
-    try {
-      data = JSON.parse(body);
-    } catch(e) {
-      data = null;
-    }
-
-    if(!data) {
-      var error
-      error = new Error('Invalid JSON from ' + url);
-      console.error(err);
-    }
-    callback(null, data, platform);
-  });
-}
-
-Worldstate.prototype.checkExpired = function(platform) {
- if(typeof this.creation === 'undefined' 
-     || this.creation.getMilliseconds() - Date.now() > maxCachedTime
-     || typeof this.data === 'undefined' 
-     || typeof this.events === 'undefined' 
-     || typeof this.sorties === 'undefined'
-     || typeof this.globalModifiers === 'undefined'
-     || typeof this.conclaveChallenge === 'undefined'
-   ){
-      this.getWorldState(platform, this.setup);
-    }
-}
-
-//Sortie
-Worldstate.prototype.getSortie = function(){
-  this.checkExpired(platform);
-  return sorties;
-}
-Worldstate.prototype.getSortieString = function(){
-  this.checkExpired(platform);
-  return sorties.toString();
-}
-
-//Conclave
-Worldstate.prototype.getConclaveDailies = function(){
-  this.checkExpired(platform);
-  return conclaveChallenge.getDailies();
-}
-Worldstate.prototype.getConclaveDailiesString = function(){
-  this.checkExpired(platform);
-  return conclaveChallenge.getDailiesAsString();
-}
-Worldstate.prototype.getConclaveWeeklies = function(){
-  this.checkExpired(platform);
-  return conclaveChallenge.getConclaveWeeklies();
-}
-Worldstate.prototype.getConclaveWeekliesString = function(){
-  this.checkExpired(platform);
-  return conclaveChallenge.getWeekliesAsString();
-}
-Worldstate.prototype.getConclaveAll = function(){
-  this.checkExpired(platform);
-  return conclaveChallenge.getConclaveAll();
-}
-Worldstate.prototype.getConclaveAllString = function(){
-  this.checkExpired(platform);
-  return conclaveChallenge.getAllAsString();
-}
-
-//Persistent enemies
-Worldstate.prototype.getAllPersistentEnemies = function() {
-  this.checkExpired(platform);
-  return enemies.getAll();
-}
-Worldstate.prototype.getAllPersistentEnemiesString = function() {
-  this.checkExpired(platform);
-  return enemies.getAllAsString();
-}
-Worldstate.prototype.getDisoveredPersistentEnemies = function() {
-  this.checkExpired(platform);
-  return enemies.getDiscovered();
-}
-Worldstate.prototype.getDisoveredPersistentEnemiesString = function() {
-  this.checkExpired(platform);
-  return enemies.getDiscoveredAsString();
-}
-Worldstate.prototype.getHiddenPersistentEnemies = function() {
-  this.checkExpired(platform);
-  return enemies.getHidden();
-}
-Worldstate.prototype.getHiddenPersistentEnemies = function() {
-  this.checkExpired(platform);
-  return enemies.getHiddenAsString();
-}
-
-//events
-Worldstate.prototype.getEvents = function() {
-  this.checkExpired(platform);
-  return events.getAll();
-}
-Worldstate.prototype.getEventsString = function() {
-  this.checkExpired(platform);
-  return events.toString();
-}
-
-//news
-Worldstate.prototype.getNews = function() {
-  this.checkExpired(platform);
-  return news.getAll();
-}
-Worldstate.prototype.getNewsString = function() {
-  this.checkExpired(platform);
-  return news.toString();
-}
-Worldstate.prototype.getUpdates = function() {
-  this.checkExpired(platform);
-  return news.getUpdates();
-}
-Worldstate.prototype.getUpdatesString = function() {
-  this.checkExpired(platform);
-  return news.getUpdatesString();
-}
-Worldstate.prototype.getPrimeAccess = function() {
-  this.checkExpired(platform);
-  return news.getPrimeAccess();
-}
-Worldstate.prototype.getPrimeAccessString = function() {
-  this.checkExpired(platform);
-  return news.getPrimeAccessString();
-}
-
-//invasions
-Worldstate.prototype.getInvasions = function(){
-  this.checkExpired(platform);
-  return invasion;
-}
-Worldstate.prototype.getInvasionsString = function(){
-  this.checkExpired(platform);
-  return invasion.toString();
-}
-
-//alerts
-Worldstate.prototype.getAlerts = function() {
-  this.checkExpired(platform);
-  return alerts.getAll();
-}
-Worldstate.prototype.getAlertsString = function () {
-  this.checkExpired(platform);
-  return alerts.getAllString();
-}
-
-//baro
-Worldstate.prototype.getVoidTrader = function() {
-  this.checkExpired(platform);
-  return voidTrader;
-}
-Worldstate.prototype.getVoidTraderString = function() {
-  this.checkExpired(platform);
-  return voidTrader.toString();
-}
-
-//darvo
-Worldstate.prototype.getDeals = function(){
-  this.checkExpired(platform);
-  return deals;
-}
-Worldstate.prototype.getDealsString = function() {
-  this.checkExpired(platform);
-  return deals.toString();
-}
-
-//flash deals
-Worldstate.prototype.getFlashDeals = function() {
-  this.checkExpired(platform);
-  return flashDeals;
-}
-Worldstate.prototype.getFlashDealsString = function() {
-  this.checkExpired(platform);
-  return flashDeals.toString();
-}
-
-//Dark sectors
-Worldstate.prototype.getDarkSectors = function() {
-  this.checkExpired(platform);
-  return darkSectors.getAll();
-}
-Worldstate.prototype.getDarkSectorsString = function() {
-  this.checkExpired(platform);
-  return darkSectors.toString();
-}
-
-//Global Modifiers
-Worldstate.prototype.getGlobalModifers = function() {
-  this.checkExpired(platform);
-  return globalModifiers.getAll();
-}
-Worldstate.prototype.getGlobalModifersString = function() {
-  this.checkExpired(platform);
-  return globalModifiers.toString();
-}
-
-module.exports = Worldstate;
+module.exports = WorldState;
