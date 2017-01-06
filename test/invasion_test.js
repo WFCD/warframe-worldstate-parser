6f4d4d09d0a7957266ba1641c02e0ef2abb34224
Matteo Rizzo <nspacestd@users.noreply.github.com>
2017-01-06 21:40:13 +0100

    Invasion fix (#39)
    * Fix invasion parsing
    
    * Update docs

--- test/invasion_test.js
+++ test/invasion_test.js
@@ -14,16 +14,23 @@ describe('Invasion', function () {
     Activation: { sec: 1000 },
     Count: 0,
     Goal: 100000,
+    AttackerMissionInfo: {
+      faction: 'faction1',
+    },
+    DefenderMissionInfo: {
+      faction: 'faction2',
+    },
   };
   const translator = {
     languageString: n => n,
     node: n => n,
+    faction: n => n,
   };
   const timeDate = {
     toNow: () => 12312,
     timeDeltaToString: () => 'timeDelta',
   };
-  const Mission = function () { this.reward = 'reward'; };
+  const Reward = function () { this.reward = 'reward'; };
 
   describe('#constructor()', function () {
     it('should throw TypeError when called with no arguments or an invalid argument', function () {
@@ -34,7 +41,7 @@ describe('Invasion', function () {
 
   describe('#toString()', function () {
     it('should choose the right format according to the factions that are fighting', function () {
-      const i = new Invasion(testData, { Mission, translator, timeDate, mdConfig });
+      const i = new Invasion(testData, { Reward, translator, timeDate, mdConfig });
       i.toString().should.match(/vs\./);
       i.vsInfestation = true;
       i.toString().should.not.match(/vs\./);
