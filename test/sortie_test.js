9a3188e5d9e0d8bc015c74ee6131b3ee2cfe6a67
Tobiah <tobiah@protonmail.com>
2017-01-30 13:12:42 -0600

    Update sortie_test.js

--- test/sortie_test.js
+++ test/sortie_test.js
@@ -29,6 +29,7 @@ describe('Sortie', function () {
     };
     const translator = {
       languageString: s => s,
+      sortieBoss: s => s,
     };
     it('should format the string correctly according to the data', function () {
       const s = new Sortie(testData, { mdConfig, timeDate, translator });
