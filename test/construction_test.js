5cba61f3e6e2b01304120969977e99a5f648abab
Tobiah <tobiah@protonmail.com>
2017-09-19 09:36:53 -0500

    Create construction_test.js

---
+++ test/construction_test.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const chai = require('chai');
+
+chai.should();
+
+const Construction = require('../lib/ConstructionProgress.js');
+const mdConfig = require('./data/markdown.json');
+
+const ProjectPct = [
+  3.0047668038409,
+  104.39419581619,
+  0,
+];
+
+const ProjectPctWithOID = {
+  ProjectPct,
+  _id: {
+    $oid: `${Date.now()}${ProjectPct[0]}`,
+  },
+};
+
+describe('Construction', () => {
+  describe('#constructor()', () => {
+    it('should throw TypeError when called with no arguments or an invalid argument', () => {
+      (() => { new Construction(); }).should.throw(TypeError);
+      (() => { new Construction({}); }).should.throw(TypeError);
+    });
+  });
+
+  describe('#toString()', () => {
+    it('should format the string correctly', () => {
+      const c = new Construction(ProjectPctWithOID, { mdConfig });
+      c.toString().should.match(/Razorback: 104\.39%/);
+      c.toString().should.match(/Fomorian: 3\.00%/);
+      c.toString().should.match(/Unknown: 0\.00%/);
+      c.fomorianProgress.should.match(/3\.00/);
+      c.razorbackProgress.should.match(/104\.39/);
+    });
+  });
+});
