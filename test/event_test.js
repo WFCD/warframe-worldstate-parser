3f0e7fc8fe605f1c082627a45154e0f129fbd6ee
Tobiah <tobiah@warframe.gg>
2020-04-08 09:48:18 -0500

    fix(arbitration): select arbitration based on time, closes #196

--- test/event_test.js
+++ test/event_test.js
@@ -7,17 +7,25 @@ chai.should();
 const Event = require('../lib/WorldEvent.js');
 const mdConfig = require('./data/markdown.json');
 const timeDate = require('./mocks/timeDate.js');
+const translation = require('./mocks/translation.js');
 
-describe('Event', function () {
-  describe('#constructor()', function () {
-    it('should throw TypeError when called with no argument or an invalid argument', function () {
+const events = require('./data/Goals.json');
+
+describe('Event', () => {
+  describe('#constructor()', () => {
+    it('should throw TypeError when called with no argument or an invalid argument', () => {
       (() => { new Event(); }).should.throw(TypeError);
       (() => { new Event({}); }).should.throw(TypeError);
     });
+
+    it('should parse mock event data', () => {
+      (() => { new Event(events, { mdConfig, timeDate, translator: translation }); })
+        .should.not.throw;
+    });
   });
-  describe('#toString()', function () {
+  describe('#toString()', () => {
     const translator = { faction: (f) => f, languageString: (l) => l, node: (n) => n };
-    it('should only include the rewards if the event has any', function () {
+    it('should only include the rewards if the event has any', () => {
       const e = new Event({ _id: { $oid: 'id' }, Expiry: { sec: 1 } }, {
         timeDate, RewardParser: {}, translator, mdConfig,
       });
@@ -26,7 +34,7 @@ describe('Event', function () {
       e.rewards = ['reward1'];
       e.toString().should.match(/Rewards/);
     });
-    it('should not include the node if the event has one', function () {
+    it('should not include the node if the event has one', () => {
       const e = new Event({ _id: { $oid: 'id' }, Expiry: { sec: 1 } }, {
         timeDate, RewardParser: {}, translator, mdConfig,
       });
@@ -35,7 +43,7 @@ describe('Event', function () {
       e.node = 'Node';
       e.toString().should.match(/Battle on/);
     });
-    it('should only include the victim node if the event has one', function () {
+    it('should only include the victim node if the event has one', () => {
       const e = new Event({ _id: { $oid: 'id' }, Expiry: { sec: 1 } }, {
         timeDate, RewardParser: {}, translator, mdConfig,
       });
