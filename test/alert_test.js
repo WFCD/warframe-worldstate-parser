c90a13092ffe289cfdeb4c1b5b21976d75ef2ac8
Tobiah <tobiah@warframe.gg>
2020-04-08 14:31:52 -0500

    chore(tests): alerts integration build test from real alerts data

--- test/alert_test.js
+++ test/alert_test.js
@@ -1,16 +1,30 @@
 'use strict';
 
 const chai = require('chai');
+const mdConfig = require('node-md-config');
 
 chai.should();
 
-const Alert = require('../lib/Alert.js');
+const Alert = require('../lib/Alert');
+const Mission = require('../lib/Mission');
+const Alerts = require('./data/Alerts');
+
+const translator = require('./mocks/translation');
+const timeDate = require('./mocks/timeDate');
 
 describe('Alert', function () {
   describe('#constructor()', function () {
     it('should throw TypeError when called with no argument or an invalid argument', function () {
       (() => { new Alert(); }).should.throw(TypeError);
       (() => { new Alert({}); }).should.throw(TypeError);
+    });
+
+    it('should successfully build alert objects when called with real data', () => {
+      (() => {
+        new Alert(Alerts, {
+          Mission, translator, timeDate, mdConfig,
+        });
+      }).should.throw(TypeError);
     });
   });
 });
