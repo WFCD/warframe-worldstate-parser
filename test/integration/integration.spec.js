12e7b71133d398a82036ce1bb1503d3aba2d3290
Tobiah <tobiah@pm.me>
2021-02-22 08:28:33 -0600

    fix(streams): pandemic streams not triggering stream flag

--- test/integration/integration.spec.js
+++ test/integration/integration.spec.js
@@ -6,6 +6,13 @@ const rewire = require('rewire');
 
 const fetch = require('node-fetch');
 
+const logger = {
+  error: () => {},
+  debug: () => {},
+  info: () => {},
+  log: () => {},
+};
+
 chai.should();
 chai.use(sinonChai);
 
@@ -18,9 +25,16 @@ describe('WorldState (integration)', () => {
         const url = `https://content.${platform === 'pc' ? '' : `${platform}.`}warframe.com/dynamic/worldState.php`;
         const ws = await fetch(url).then((d) => d.text());
 
+        let wsl;
         (() => {
-          new WorldState(ws);
+          wsl = new WorldState(ws, { logger });
         }).should.not.throw();
+
+        wsl.news.forEach((article) => {
+          if (article.message.toLowerCase().includes('stream')) {
+            article.should.include({ stream: true });
+          }
+        });
       });
     });
   });
