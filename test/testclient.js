168f78f573e72f7e6e073771069a3d0b6c9c1508
Tobiah <tobiah@pm.me>
2021-07-21 15:08:20 +0000

    fix: constrain arbi times better

---
+++ test/testclient.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const fetch = require('node-fetch');
+const Worldstate = require('../main');
+
+/* eslint-disable no-console */
+
+fetch('https://10o.io/arbitrations.json')
+  .then((res) => res.json())
+  .then(async (semlar) => ({
+    semlar,
+    ws: await fetch('https://content.warframe.com/dynamic/worldState.php').then((res) => res.text()),
+  }))
+  .then(({ semlar, ws }) => {
+    const parsed = new Worldstate(ws, { kuvaData: semlar });
+    console.log(parsed.arbitration);
+    process.exit(0);
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
