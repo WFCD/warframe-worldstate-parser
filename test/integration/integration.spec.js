b9047f46a4cc66470e1fdac1fd5d689289b43075
Matej Voboril <tobiah@pm.me>
2021-12-16 23:44:34 -0600

    fix: improve narmer drop matching & timebinding

--- test/integration/integration.spec.js
+++ test/integration/integration.spec.js
@@ -22,26 +22,30 @@ const WorldState = rewire('../../lib/WorldState.js');
 describe('WorldState (integration)', () => {
   describe('#constructor()', async () => {
     ['pc', 'ps4', 'xb1', 'swi'].forEach((platform) => {
-      it('should parse live worldstate data', async function () {
+      it('should parse live worldstate data', function (done) {
         this.timeout = 10000; // allow 10 seconds to parse the worldstate
         const url = `https://content.${platform === 'pc' ? '' : `${platform}.`}warframe.com/dynamic/worldState.php`;
-        const ws = await fetch(url).then((d) => d.text());
+        fetch(url)
+          .then((d) => d.text())
+          .then((ws) => {
+            let wsl;
+            (() => {
+              wsl = new WorldState(ws, { logger, locale: 'en' });
+            }).should.not.throw();
 
-        let wsl;
-        (() => {
-          wsl = new WorldState(ws, { logger, locale: 'zh' });
-        }).should.not.throw();
+            wsl.news.forEach((article) => {
+              if (article.message.toLowerCase().includes('stream')) {
+                article.should.include({ stream: true });
+              }
+            });
 
-        wsl.news.forEach((article) => {
-          if (article.message.toLowerCase().includes('stream')) {
-            article.should.include({ stream: true });
-          }
-        });
-
-        /* Easy debugging! */
-        setTimeout(() => {
-          fs.writeFileSync(`./data.${platform}.json`, JSON.stringify(wsl));
-        }, 1000);
+            /* Easy debugging! */
+            setTimeout(() => {
+              fs.writeFileSync(`./data.${platform}.json`, JSON.stringify(wsl.syndicateMissions
+                .find((m) => m.syndicateKey === 'Ostrons')));
+              done();
+            }, 1000);
+          });
       });
     });
   });
