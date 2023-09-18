e97ee37fbd9fc7fdedda0b5826b4226575e4f196
Matt <7128721+TobiTenno@users.noreply.github.com>
2023-09-18 17:43:55 -0500

    feat: add duviri choices (#466)

--- test/integration/integration.spec.js
+++ test/integration/integration.spec.js
@@ -14,54 +14,44 @@ const logger = {
   log: () => {},
 };
 
+const { expect } = chai;
 chai.should();
 chai.use(sinonChai);
 
 const WorldState = require('../../lib/WorldState');
 
 describe('WorldState (integration)', () => {
-  describe('#constructor()', async () => {
-    await Promise.all(
-      // no more other console worldstates.....
-      ['pc'].map(async function (platform) {
-        it(`should parse live ${platform} worldstate data`, async function () {
-          this.timeout = 10000; // allow 10 seconds to parse the worldstate
+  it(`should parse live pc worldstate data`, async function () {
+    this.timeout = 10000; // allow 10 seconds to parse the worldstate
+    const kuvaData = await fetch('https://10o.io/arbitrations.json').then((res) => res.json());
+    const ws = await fetch('https://content.warframe.com/dynamic/worldState.php').then((res) => res.text());
 
-          const { kuvaData, ws } = await fetch('https://10o.io/arbitrations.json')
-            .then((res) => res.json())
-            .then(async (semlar) => ({
-              kuvaData: semlar,
-              ws: await fetch('https://content.warframe.com/dynamic/worldState.php').then((res) => res.text()),
-            }));
+    let wsl;
+    (() => {
+      try {
+        // once without kuva data
+        wsl = new WorldState(ws, { logger, locale: 'en' });
+        wsl = new WorldState(ws, { logger, locale: 'en', kuvaData });
+      } catch (e) {
+        console.error(e); // eslint-disable-line no-console
+        throw e;
+      }
+    }).should.not.throw();
 
-          let wsl;
-          (() => {
-            try {
-              // once without kuva data
-              wsl = new WorldState(ws, { logger, locale: 'en' });
-              wsl = new WorldState(ws, { logger, locale: 'en', kuvaData });
-            } catch (e) {
-              console.error(e); // eslint-disable-line no-console
-              throw e;
-            }
-          }).should.not.throw();
-
-          wsl &&
-            wsl.news &&
-            wsl.news.forEach((article) => {
-              if (article.message.toLowerCase().includes('stream')) {
-                article.should.include({ stream: true });
-              }
-            });
-          /* Easy debugging! */
-          if (process.env.CI) {
-            return fs.writeFile(
-              path.resolve(`./data.${platform}.json`),
-              JSON.stringify(wsl.syndicateMissions.find((m) => m.syndicateKey === 'Ostrons'))
-            );
-          }
-        });
-      })
-    );
+    expect(wsl.news).to.exist;
+    wsl?.news?.forEach((article) => {
+      if (article.message.toLowerCase().includes('stream')) {
+        article.should.include({ stream: true });
+      }
+    });
+    expect(wsl?.duviriCycle).to.exist;
+    wsl?.duviriCycle?.choices.should.have.length.gte(2);
+    /* Easy debugging! */
+    if (process.env.CI) {
+      return fs.writeFile(
+        path.resolve(`./data.pc.json`),
+        JSON.stringify(wsl.syndicateMissions.find((m) => m.syndicateKey === 'Ostrons'))
+      );
+    }
   });
 });
