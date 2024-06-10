2b05c87f28cbfac517e37e643debf5416d70d6c1
Ornstein <6075693+SlayerOrnstein@users.noreply.github.com>
2024-06-10 16:46:06 -0400

    fix: modified sentient outpost regex to only target the `sfn` key-value (#530)

---
+++ test/unit/sentientOutpost.spec.js
@@ -0,0 +1,27 @@
+import * as chai from 'chai';
+import sinonChai from 'sinon-chai';
+
+import SentientOutpost from '../../lib/models/SentientOutpost.js';
+import data from '../data/Tmp.js';
+
+chai.should();
+chai.use(sinonChai);
+
+describe('SentientOutpost', function () {
+  describe('#constructor()', function () {
+    it('should be able to handle some raw data', () => {
+      const outpost = new SentientOutpost(data, { locale: 'en', logger: console });
+
+      outpost.id.should.equal('CrewBattleNode554:true');
+      outpost.mission.node.should.equal('H-2 Cloud (Veil)');
+    });
+    it('should throw TypeError when called with no argument or an invalid argument', function () {
+      (() => {
+        new SentientOutpost();
+      }).should.throw(TypeError);
+      (() => {
+        new SentientOutpost({});
+      }).should.throw(TypeError);
+    });
+  });
+});
