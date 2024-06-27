2e3eef9ad330777c12ec10bc8b27148c4c9e8217
Matt <7128721+TobiTenno@users.noreply.github.com>
2024-06-26 19:26:15 -0500

    fix(sec): redos, release (#543)

--- test/testclient.js
+++ test/testclient.js
@@ -1,19 +1,15 @@
-import Worldstate from '../main.js';
+import WorldState from 'warframe-worldstate-parser';
 
 /* eslint-disable no-console */
 
-fetch('https://10o.io/arbitrations.json')
-  .then((res) => res.json())
-  .then(async (semlar) => ({
-    semlar,
-    ws: await fetch('https://content.warframe.com/dynamic/worldState.php').then((res) => res.text()),
-  }))
-  .then(({ semlar, ws }) => {
-    const parsed = new Worldstate(ws, { kuvaData: semlar });
-    console.log(parsed.duviriCycle.toString());
-    process.exit(0);
-  })
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  });
+try {
+  const semlar = await fetch('https://10o.io/arbitrations.json').then((res) => res.json());
+  const ws = await fetch('https://content.warframe.com/dynamic/worldState.php').then((res) => res.text());
+
+  const parsed = await WorldState.build(ws, { kuvaData: semlar });
+  console.log(parsed.duviriCycle.toString());
+  process.exit(0);
+} catch (e) {
+  console.error(e);
+  process.exit(1);
+}
