aaf6f6fa9beddd65f320ad8c6ae7a7b0d74079dd
SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>
2024-10-07 20:36:07 -0400

    chore: parse Tmp outside of SentientOutpost

--- test/unit/sentientOutpost.spec.js
+++ test/unit/sentientOutpost.spec.js
@@ -2,7 +2,6 @@ import * as chai from 'chai';
 import sinonChai from 'sinon-chai';
 
 import SentientOutpost from '../../lib/models/SentientOutpost.js';
-import data from '../data/Tmp.js';
 
 chai.should();
 chai.use(sinonChai);
@@ -10,17 +9,17 @@ chai.use(sinonChai);
 describe('SentientOutpost', function () {
   describe('#constructor()', function () {
     it('should be able to handle some raw data', () => {
-      const outpost = new SentientOutpost(data, { locale: 'en', logger: console });
+      const outpost = new SentientOutpost(554, { locale: 'en', logger: console });
 
       outpost.id.should.equal('CrewBattleNode554:true');
       outpost.mission.node.should.equal('H-2 Cloud (Veil)');
     });
     it('should throw TypeError when called with no argument or an invalid argument', function () {
       (() => {
-        new SentientOutpost();
+        SentientOutpost();
       }).should.throw(TypeError);
       (() => {
-        new SentientOutpost({});
+        SentientOutpost({});
       }).should.throw(TypeError);
     });
   });
