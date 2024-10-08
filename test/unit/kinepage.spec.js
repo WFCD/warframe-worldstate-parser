5e09cc0478ef67209f4979092dc23c81dd6bda36
SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>
2024-10-07 21:56:57 -0400

    chore: cleanup test data

--- test/unit/kinepage.spec.js
+++ test/unit/kinepage.spec.js
@@ -9,20 +9,7 @@ chai.use(sinonChai);
 const data = {
   ts: '1727450100',
   en: 'Amir we talked about this',
-  fr: 'Amir, on en a parlé',
-  it: 'Amir ne abbiamo già parlato',
-  de: 'Amir, wir haben darüber geredet',
   es: 'Amir, ya hablamos de esto',
-  pt: 'Amir, já falamos sobre isso',
-  ru: 'Амир, мы говорили об этом.',
-  pl: 'Amir gadalismy o tym',
-  uk: 'Аміре, ми про це говорили',
-  tr: 'Amir bunu konuşmuştuk',
-  ja: '前にも言ったろAmir',
-  zh: '阿米尔，我们讨论过这个问题了',
-  ko: '아미르 예전에 얘기 안 했냐',
-  tc: '阿米爾，我們談過這個了',
-  th: 'Amir we talked about this',
 };
 
 describe('Kinepage', function () {
