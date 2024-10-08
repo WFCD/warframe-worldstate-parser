d475ac21b20568922fd204b1411ef6ba273233db
SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>
2024-10-08 18:31:10 -0400

    feat: added Deep Archimedea

---
+++ test/unit/deeparchimedea.spec.js
@@ -0,0 +1,26 @@
+import * as chai from 'chai';
+import sinonChai from 'sinon-chai';
+
+import DeepArchidemea from '../../lib/models/DeepArchidemea.js';
+import data from '../data/DeepArchimedea.json' assert { type: 'json' };
+
+chai.should();
+chai.use(sinonChai);
+
+describe('SentientOutpost', function () {
+  describe('#constructor()', function () {
+    it('should be able to handle some raw data', () => {
+      const archimedea = new DeepArchidemea(Date.now(), Date.now(), data);
+
+      archimedea.missions[0].mission.should.equal('Extermination');
+    });
+    it('should throw TypeError when called with no argument or an invalid argument', function () {
+      (() => {
+        new DeepArchidemea();
+      }).should.throw(TypeError);
+      (() => {
+        new DeepArchidemea({});
+      }).should.throw(TypeError);
+    });
+  });
+});
