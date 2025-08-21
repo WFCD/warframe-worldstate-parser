8bdfe29613f4abc410dbfd215adffcf244512299
SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>
2025-08-21 12:38:31 -0400

    chore: go back to how it was before for test client

--- test/testclient.ts
+++ test/testclient.ts
@@ -1,12 +1,10 @@
-import WorldState from '../lib/WorldState';
-
-/* eslint-disable no-console */
+import WorldState from 'warframe-worldstate-parser';
 
 try {
   const semlar = await fetch('https://10o.io/arbitrations.json').then((res) => res.json());
   const ws = await fetch('https://content.warframe.com/dynamic/worldState.php').then((res) => res.text());
 
-  const parsed = await WorldState(ws, { locale: 'en', kuvaData: semlar });
+  const parsed = await WorldState.build(ws, { locale: 'en', kuvaData: semlar });
   console.log(parsed.duviriCycle.toString());
   process.exit(0);
 } catch (e) {
