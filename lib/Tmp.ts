e05b6c906ed2e5d9ceda77fd6f5f5bb61494a118
SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>
2025-10-15 21:34:10 -0400

    fix: refactor to use new archimedea structure

--- lib/Tmp.ts
+++ lib/Tmp.ts
@@ -1,5 +1,3 @@
-import type { RawArchimedea } from './models/Archidemea';
-import Archimedea from './models/Archidemea';
 import Kinepage from './models/Kinepage';
 import SentientOutpost from './models/SentientOutpost';
 import type Dependency from './supporting/Dependency';
@@ -8,8 +6,6 @@ export interface InitialTmp {
   sfn: number;
   pgr: { [k: string]: string | number };
   fbst?: { a: number; e: number; n: number };
-  lqo?: RawArchimedea;
-  hqo?: RawArchimedea;
   QTCCFloofCount?: number;
   QTCCFloofLimit?: number;
 }
@@ -17,8 +13,6 @@ export interface InitialTmp {
 export class Tmp {
   sentientOutposts: SentientOutpost;
   kinepage: Kinepage;
-  deepArchimedea?: Archimedea;
-  temporalArchimedea?: Archimedea;
   faceoffBonus?: { activation: Date; expiry: Date; next: Date };
   questToConquerCancer?: { count: number; goal: number };
 
@@ -36,14 +30,6 @@ export class Tmp {
         expiry: toDate(tmp.fbst.e),
         next: toDate(tmp.fbst.n),
       };
-    }
-
-    if (tmp.lqo) {
-      this.deepArchimedea = new Archimedea(tmp.lqo, deps.locale);
-    }
-
-    if (tmp.hqo) {
-      this.temporalArchimedea = new Archimedea(tmp.hqo, deps.locale);
     }
 
     if (tmp.QTCCFloofCount) {
