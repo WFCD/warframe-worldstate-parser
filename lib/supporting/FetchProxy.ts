76b7c935e649feb7b14e58b028e9c8d8f195a3b7
Matej Voboril <tobiah@pm.me>
2025-10-15 20:44:06 -0500

    fix: some proxy for tests

---
+++ lib/supporting/FetchProxy.ts
@@ -0,0 +1,61 @@
+const proxyUrl = process.env.PROXY_URL ?? undefined;
+const isCI = process.env.CI === 'true';
+const ciTimeout = process.env.CI_TIMEOUT ? parseInt(process.env.CI_TIMEOUT, 10) : 60000;
+const localTimeout = process.env.LOCAL_TIMEOUT ? parseInt(process.env.LOCAL_TIMEOUT, 10) : 12000000;
+
+export default async (url: string, {
+  session = 'parser-session', contentType = 'application/json',
+}: {
+  session?: string;
+  contentType?: 'application/json' | 'text/html';
+} = {
+  session: 'parser-session', contentType: 'application/json',
+}) => {
+  if (proxyUrl) {
+    const res = await fetch(`${proxyUrl}/v1`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        cmd: 'request.get',
+        url,
+        session,
+        maxTimeout: isCI ? ciTimeout : localTimeout,
+        returnOnlyCookies: false,
+        returnPageContent: true,
+      }),
+    });
+    const text = await res.text();
+    const { solution } = JSON.parse(text);
+    if (!solution?.response) {
+      throw solution;
+    }
+    if (contentType === 'application/json') {
+      return {
+        ok: solution.responseCode === 200,
+        status: solution.responseCode,
+        json: async () => {
+          try {
+            return JSON.parse(solution.response);
+          } catch (error) {
+            return {};
+          }
+        },
+      };
+    }
+    if (contentType === 'text/html') {
+      return {
+        ok: solution.responseCode === 200,
+        status: solution.responseCode,
+        text: async () => solution.response.replace('<html>', '')
+          .replace('<body>', '')
+          .replace('<head>', '')
+          .replace('</html>', '')
+          .replace('</body>', '')
+          .replace('</head>', ''),
+      }
+    }
+    return solution.response;
+  } else {
+    return fetch(url);
+  }
+}
--- test/testclient.ts
+++ test/testclient.ts
@@ -1,17 +1,14 @@
 import WorldState from '../dist/main.js';
+import fetch from '../dist/lib/supporting/FetchProxy.js';
 
-const run = async () => {
-  try {
-    const semlar = await fetch('https://10o.io/arbitrations.json').then((res) => res.json());
-    const ws = await fetch('https://api.warframe.com/cdn/worldState.php').then((res) => res.text());
+try {
+  const semlar = await fetch('https://10o.io/arbitrations.json').then((res) => res.json());
+  const ws = (await fetch('https://api.warframe.com/cdn/worldState.php', { contentType: 'text/html' }).then((res) => res.text()));
 
-    const parsed = await WorldState.build(ws, { locale: 'en', kuvaData: semlar });
-    console.log(parsed.duviriCycle.toString());
-    process.exit(0);
-  } catch (e) {
-    console.error(e);
-    process.exit(1);
-  }
-};
-
-run();
+  const parsed = await WorldState.build(ws, { locale: 'en', kuvaData: semlar });
+  console.log(parsed.fissures.toString());
+  process.exit(0);
+} catch (e) {
+  console.error(`it errored: ${JSON.stringify(e)}`);
+  process.exit(1);
+}
